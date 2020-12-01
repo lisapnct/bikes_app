bdfb23886644b8b48782ca56bffb0ecaf5e4b6e6
Lisa Poncet <poncet.lisa@gmail.com>
2020-12-01 14:51:21 +0100

    turn OneBike into hook

--- client/src/pages/OneBike.jsx
+++ client/src/pages/OneBike.jsx
@@ -1,53 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import apiHandler from "../apiHandler.js";
 import { Link } from "react-router-dom";
 import { IconContext } from "react-icons";
 
-class OneBike extends React.Component {
-  state = {
-    bike: null,
-  };
+const OneBike = (props) => {
+  const [bike, setBike] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     apiHandler
-      .getOne("/bikes/", this.props.match.params.id)
-      .then((apiRes) =>
-        this.setState({
-          bike: apiRes.data,
-        })
-      )
+      .getOne("/bikes/", props.match.params.id)
+      .then((apiRes) => setBike(apiRes.data))
       .catch((err) => console.log(err));
-  }
+  }, [props.match.params.id]);
 
-  handleDelete = () => {
+  const handleDelete = () => {
     apiHandler
-      .deleteOne("/bikes/", this.state.bike._id)
+      .deleteOne("/bikes/", bike._id)
       .then(() => {
-        this.props.history.push("/bikes");
+        props.history.push("/bikes");
       })
       .catch((err) => console.log(err));
   };
 
-  render() {
-    if (!this.state.bike) {
-      return <div>Loading...</div>;
-    }
-    return (
-      <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
-        <div>
-          <h1>{this.state.bike.owner}'s bike</h1>
-          <img src={this.state.bike.image} alt="owner-bike-img" />
-          <h3>Price: ${this.state.bike.price}</h3>
-          <button className="action-btn">
-            <Link to={`/bikes/${this.state.bike._id}/edit`}>edit</Link>
-          </button>
-          <button className="action-btn" onClick={this.handleDelete}>
-            delete
-          </button>
-        </div>
-      </IconContext.Provider>
-    );
+  if (!bike) {
+    return <div>Loading...</div>;
   }
-}
+  return (
+    <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
+      <div>
+        <h1>{bike.owner}'s bike</h1>
+        <img src={bike.image} alt="owner-bike-img" />
+        <h3>Price: ${bike.price}</h3>
+        <button className="action-btn">
+          <Link to={`/bikes/${bike._id}/edit`}>edit</Link>
+        </button>
+        <button className="action-btn" onClick={handleDelete}>
+          delete
+        </button>
+      </div>
+    </IconContext.Provider>
+  );
+};
 
 export default OneBike;
