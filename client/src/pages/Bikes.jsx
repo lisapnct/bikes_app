d65cd68c56150b4a3f406556a0f7db3f086e1064
Lisa Poncet <poncet.lisa@gmail.com>
2020-12-01 15:00:33 +0100

    turn Bikes.js into hooks

--- client/src/pages/Bikes.jsx
+++ client/src/pages/Bikes.jsx
@@ -1,51 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import apiHandler from "../apiHandler.js";
 import { Link } from "react-router-dom";
 
 import "../styles/Bikes.css";
 
-class Bikes extends React.Component {
-  state = {
-    bikes: [],
-  };
+const Bikes = () => {
+  const [bikes, setBikes] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     apiHandler
       .getAll("/bikes")
-      .then((apiRes) => {
-        // console.log(apiRes);
-        this.setState({
-          bikes: apiRes.data,
-        });
-      })
+      .then((apiRes) => setBikes(apiRes.data))
       .catch((err) => console.log(err));
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <h1>all bikes_</h1>
-        <div className="bikes-container">
-          {this.state.bikes.map((bike) => (
-            <div key={bike._id} className="bike-card">
-              <h3>
-                {bike.brand} - ${bike.price}
-              </h3>
-              <h4>{bike.owner} is the owner</h4>
-              <p>
-                <b>Color: </b>
-                {bike.color} <b> | Size: </b>
-                {bike.size}
-              </p>
-              <Link to={`/bikes/${bike._id}`}>
-                <button className="action-btn details">details</button>
-              </Link>
-            </div>
-          ))}
-        </div>
+  return (
+    <div>
+      <h1>all bikes_</h1>
+      <div className="bikes-container">
+        {bikes.map((bike) => (
+          <div key={bike._id} className="bike-card">
+            <h3>
+              {bike.brand} - ${bike.price}
+            </h3>
+            <h4>{bike.owner} is the owner</h4>
+            <p>
+              <b>Color: </b>
+              {bike.color} <b> | Size: </b>
+              {bike.size}
+            </p>
+            <Link to={`/bikes/${bike._id}`}>
+              <button className="action-btn details">details</button>
+            </Link>
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Bikes;
